93629f67685ae12e28c5b9f358e58fbd340c12b5
alexismrosales <aleisjorge91@gmail.com>
2023-12-14 00:26:06 -0600

    cards fixed

--- src/home.js
+++ src/home.js
@@ -75,7 +75,7 @@ export default function Home(){
                             </div>
                             <div className='col-sm-12 col-md-6 col-lg-4 pt-4'>
                                 <div className='card'>
-                                    <img src="..." className="card-img-top" alt="..."/>
+                                    <img src={photo3} className="card-img-top" alt="..."/>
                                     <div className="card-body">
                                         <h5 className="card-title">Card title</h5>
                                         <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
