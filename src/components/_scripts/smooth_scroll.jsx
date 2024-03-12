f9bf32f77ffa8ff1d42353110935d7b066afac86
alexismrosales <aleisjorge91@gmail.com>
2024-03-11 18:17:36 -0600

    created smooth scroll script

---
+++ src/components/_scripts/smooth_scroll.jsx
@@ -0,0 +1,13 @@
+
+export default function smoothScroll(classSelector) {
+    const scrollLinks = document.querySelectorAll(classSelector);
+
+    scrollLinks.forEach((link) => {
+      link.addEventListener("click", (event) => {
+        event.preventDefault();
+        const href = link.getAttribute("href");
+        const targetElement = document.querySelector(href);
+        targetElement.scrollIntoView({ behavior: "smooth" });
+      });
+    });
+  }
