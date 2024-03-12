f25689709be99d683bf9a57ab7f3fff12f868f69
alexismrosales <aleisjorge91@gmail.com>
2024-03-12 05:05:09 -0600

    updated cards sections

--- src/components/projects&programs/index.tsx
+++ src/components/projects&programs/index.tsx
@@ -1,22 +1,44 @@
 import { useTranslation } from "react-i18next";
 
+import { ProjectsI } from "../../data/Projects";
+import { ProgramsI } from "../../data/Programs";
+
+import ProjectData from "../../data/Projects";
+import ProgramData from "../../data/Programs";
+
+import Card from "../cards";
 import style from './styles.module.css'
+
 import classNames from "classnames";
+import { FunctionComponent } from "react";
 const Projects = () => {
     const {t} = useTranslation();
     return (
     <>
         <section id="Projects">
             <div className={classNames(style.container,'bg-white')}>
-                xd
+                <ListCards elements={ProjectData} type={"Project"} id="Project"/>
             </div>
         </section>
         <section id="Programs">
             <div className={classNames(style.container,'bg-[#4949da]')}>
-                xd
+                <ListCards elements={ProgramData} type={"Program"} id="Program"/>
             </div>
         </section>
     </>
     );
 }
+
+const ListCards : FunctionComponent<{
+    elements : ProjectsI | ProgramsI,
+    type : "Project" | "Program",
+    id : string
+}> = props => (
+    <div className={style.container} id={props.id}>
+        {Object.entries(props.elements).map(([name, Element]) => (
+        <Card key={name} name={name} element={Element} type={props.type} />
+      ))}
+    </div>
+)
+
 export default Projects;
