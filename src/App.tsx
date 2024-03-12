870c5aa7d2e891aa496b1c5a9b81a80c22e9dc2e
alexismrosales <aleisjorge91@gmail.com>
2024-03-11 18:11:52 -0600

    created projects component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import Header from './components/header';
 import Intro from './components/intro';
+import Projects from './components/projects';
 import AboutMe from './components/aboutme';
 function App() {
   return (
     <div className="App">
       <Header/>
       <Intro/>
+      <Projects/>
       <AboutMe/>
     </div>
   );
