6d4681df6ad3085ccd9c9d75ddf0ed892f92d810
alexismrosales <aleisjorge91@gmail.com>
2024-03-13 16:08:48 -0600

    updated data and style

--- src/components/aboutme/index.tsx
+++ src/components/aboutme/index.tsx
@@ -1,12 +1,24 @@
 import { useTranslation } from "react-i18next";
 
+import AboutMeData from "../../data/Aboutme";
+
 import style from './styles.module.css'
 const AboutMe = () => {
-    const {t} = useTranslation();
+    const {t, i18n} = useTranslation();
+    const title: JSX.Element = i18n.language === "en" ? AboutMeData.en.Title : AboutMeData.es.Title;
+    const text: JSX.Element = i18n.language === "en" ? AboutMeData.en.Text : AboutMeData.es.Text;
+    const phrase: JSX.Element = i18n.language === "en" ? AboutMeData.en.Phrase : AboutMeData.es.Phrase;
     return (
     <section id="AboutMe">
-        <div className={style.container}>
-            xd
+        <div className={style.container}>   
+            <div className={style.aboutMeImage}></div>
+            <div className={style.aboutMeText}>
+                <h2 className={style.title}>{title}.</h2>
+                <br/>
+                <p className={style.text}>{text}</p>
+                <br/>
+                <p className={style.phrase}>{phrase}</p>
+            </div>
         </div>
     </section>
     );
