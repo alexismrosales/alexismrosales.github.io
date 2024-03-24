041a93068d5c91ff56d2e601f807b92f336159f3
rarksz <aleisjorge91@gmail.com>
2024-03-24 04:21:30 -0600

    responsive cards fixed

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
     "./**/@material-tailwind/**/*.{html,js,ts,jsx,tsx,mdx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        '2xl': { 'min': '1800px' },
+      }
+    }
   },
   plugins: [],
 }
